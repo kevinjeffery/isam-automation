4ce7c433675c9ed1dd77ffaa07ed2f493e23a141
kevinjeffery <kevin@microknight.com>
2020-09-26 11:58:57 -0400

    Optimize select statement.

--- scripts/cleanup_rba_device.sql
+++ scripts/cleanup_rba_device.sql
@@ -1,5 +1,5 @@
 -- cleanup_rba_device.sql
--- v2.00_2020-08-27
+-- v2.01_2020-09-26
 -- Kevin Jeffery
 -- Example: CALL CLEANUP_RBA_DEVICE(1000000, 100, ?, ?, ?)
 
@@ -15,11 +15,10 @@ AUTONOMOUS
 BEGIN
 DECLARE target TIMESTAMP;
 
-SET target = CURRENT TIMESTAMP;
+SET target = CURRENT TIMESTAMP - deviceExpirationTime DAYS;
 
 SELECT count(*) INTO rowCount FROM RBA_DEVICE;
-SELECT count(*) INTO expiredCount FROM RBA_DEVICE
-WHERE timestampdiff(16, char(target - LAST_USED_TIME)) > deviceExpirationTime;
+SELECT count(*) INTO expiredCount FROM RBA_DEVICE WHERE LAST_USED_TIME < target;
 SET cleanupCount = 0;
 IF expiredCount < maxCount THEN
     SET maxCount = expiredCount;
@@ -29,7 +28,7 @@ L0: WHILE (cleanupCount < maxCount) DO
     L1: FOR expireID AS
     SELECT DEVICE_ID
     FROM RBA_DEVICE
-    WHERE timestampdiff(16, char(target - LAST_USED_TIME)) > deviceExpirationTime FETCH FIRST 1000 ROWS ONLY
+    WHERE LAST_USED_TIME < target FETCH FIRST 1000 ROWS ONLY
     DO
         DELETE FROM RBA_DEVICE WHERE DEVICE_ID = expireID.DEVICE_ID;
         SET cleanupCount = cleanupCount + 1;
